e438f6621cc7b129de59cec34aef609e56e4a3eb
maskit353 <zagnetoff@gmail.com>
2017-11-16 02:01:16 +0300

    renamed variables, initialization in functions

--- ToDoList/javascript.js
+++ ToDoList/javascript.js
@@ -1,65 +1,101 @@
-/*
-В компании с Егором Залевским, совместный проект ToDoList
-*/
-var Tasks = [];
+var domBuilderModule = (function () {
+	var addTaskBtn, deleteTaskBtn, checkAllBtn;
+	var newTask;
+	var taskListHTML;
+	var tasks;
+	var checkboxes;
 
-function add(){
-	var addTask = document.getElementById("text").value;
-	var spaceCounter = 0;
-	for(var i = 0; i < addTask.length; i++){
-		if(addTask[i] == " "){
-			spaceCounter++;
+	function init() {
+		initControls();
+		initListeners();
+		initTaskList();
+		initCheckboxes();
+		initTaskListHTML();
+		initNewTask();
+	}
+
+	function initControls(){
+		addTaskBtn = document.getElementById("add");
+		deleteTaskBtn = document.getElementById("delete");
+		checkAllBtn = document.getElementById("check");
+	}
+
+	function initListeners(){
+		addTaskBtn.addEventListener("click", addTask);
+		deleteTaskBtn.addEventListener("click", deleteTask);
+		checkAllBtn.addEventListener("click", checkAll);
+	}
+
+	function initTaskList(){
+		tasks = [];
+	}
+
+	function initCheckboxes(){
+		checkboxes = [];
+	}
+
+	function initTaskListHTML(){
+		taskListHTML = document.getElementById("taskListParser");
+	}
+
+	function initNewTask(){
+		newTask = document.getElementById("newTask");
+	}
+
+	function addTask(){
+		tasks.push(newTask.value);
+		repaint();
+	}
+
+	function deleteTask(){
+		var bufArray = [];
+		var size = tasks.length;
+		for(var i = 0; i < size; i++){
+			var checkbox = document.getElementById(i);
+			if(checkbox.checked == false){
+				bufArray.push(tasks[i]);
+			}
+		}
+		tasks = bufArray;
+		repaint();
+	}
+
+	function repaint(){
+		taskListHTML.innerHTML = '';
+			for(var i = 0; i < tasks.length; i++){
+				taskListHTML.innerHTML += '<p>' + '<input type = "checkbox" ' + 'id="' + i +'">' + tasks[i] + '</p>' ;
+			}
+	}
+
+	function checkAll(){
+		var checkCounter = 0;
+		for(var i = 0; i < tasks.length; i++){
+			if(document.getElementById(i).checked == true){
+				checkCounter++;
+			}
+		}
+		if(checkCounter == tasks.length){
+			for(var i = 0; i < tasks.length; i++){
+			document.getElementById(i).checked = false;
+			}
+		}
+		else{
+			for(var i = 0; i < tasks.length; i++){
+				document.getElementById(i).checked = true;
+			}
 		}
 	}
-	if(spaceCounter != addTask.length){
-		var div = document.getElementById("task");
-		var Task = {task: addTask};
-		Tasks.push(Task);
-		repaint(div);
-		document.getElementById("text").value = '';
-	}
-	else{
-		alert("Введите что-нибудь");
-	}
 
+	return {
+       init: init
+    };
+ })(); 
+
+(function () {
+    window.onload = initTodoList;
+	function initTodoList() {
+	  domBuilderModule.init();
+	}
 }
+)();
 
-function deleteTask(){
-	var bufArray = [];
-	var size = Tasks.length;
-	var div = document.getElementById("task");
-	for(var i = 0; i < size; i++){
-		var checkbox = document.getElementById(i);
-		if(checkbox.checked == false){
-			bufArray.push(Tasks[i]);
-		}
-	}
-	Tasks = bufArray;
-	repaint(div);
-}
-
-function repaint(div){
-	div.innerHTML = '';
-		for(var i = 0; i < Tasks.length; i++){
-			div.innerHTML += '<p>' + '<input type = "checkbox" ' + 'id="' + i +'">' + Tasks[i].task + '</p>' ;
-		}
-}
-
-function checkAll(){
-	var checkNum = 0;
-	for(var i = 0; i < Tasks.length; i++){
-		if(document.getElementById(i).checked == true){
-			checkNum++;
-		}
-	}
-	if(checkNum == Tasks.length){
-		for(var i = 0; i < Tasks.length; i++){
-		document.getElementById(i).checked = false;
-		}
-	}
-	else{
-		for(var i = 0; i < Tasks.length; i++){
-			document.getElementById(i).checked = true;
-		}
-	}
-}
